999f518d79bf51e5b3752e937466556984227eb3
Isidora Aleksic <isidora123456666@gmail.com>
2024-09-26 10:58:59 +0200

    Loading recommended movies and adding pagination.

--- movie-recommendation-system/src/app/movie-management/movie-sorting-page/movie-sorting-page.component.ts
+++ movie-recommendation-system/src/app/movie-management/movie-sorting-page/movie-sorting-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Movie } from 'src/app/model';
 import { MovieService } from '../movie.service';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-movie-sorting-page',
@@ -9,192 +9,70 @@ import { Router } from '@angular/router';
   styleUrls: ['./movie-sorting-page.component.css']
 })
 export class MovieSortingPageComponent implements OnInit{
-  sortResults() {
-  }
-  
   movies: Movie [] = [];
+  movie: Movie | undefined;
+  currentPage: number = 1;
+  isSorted:boolean=false;
 
-  constructor(private movieService: MovieService, private router:Router) { }
-
-  private fallbackMovies: Movie[] = [
-    {
-      budget: 100000000,
-      genres: [{ id: 28, name: 'Action' }, { id: 12, name: 'Adventure' }],
-      homepage: 'http://example.com',
-      id: 1,
-      keywords: [{ id: 1, name: 'example' }],
-      original_language: 'en',
-      original_title: 'Fallback Movie 1',
-      overview: 'This is a fallback movie.',
-      popularity: 10,
-      production_companies: [{ id: 1, name: 'Example Production' }],
-      production_countries: [{ iso_3166_1: 'US', name: 'United States' }],
-      release_date: '2024-01-01',
-      revenue: 5000000,
-      runtime: 120,
-      spoken_languages: [{ iso_639_1: 'en', name: 'English' }],
-      status: 'Released',
-      tagline: 'An example fallback movie.',
-      title: 'Fallback Movie 1',
-      vote_average: 7.0,
-      vote_count: 1000
-    },
-    {
-      budget: 200000000,
-      genres: [{ id: 16, name: 'Animation' }, { id: 35, name: 'Comedy' }],
-      homepage: 'http://example.com',
-      id: 2,
-      keywords: [{ id: 2, name: 'example' }],
-      original_language: 'en',
-      original_title: 'Fallback Movie 2',
-      overview: 'This is another fallback movie.',
-      popularity: 15,
-      production_companies: [{ id: 2, name: 'Example Studios' }],
-      production_countries: [{ iso_3166_1: 'GB', name: 'United Kingdom' }],
-      release_date: '2024-02-01',
-      revenue: 10000000,
-      runtime: 90,
-      spoken_languages: [{ iso_639_1: 'es', name: 'Spanish' }],
-      status: 'Released',
-      tagline: 'Another example fallback movie.',
-      title: 'Fallback Movie 2',
-      vote_average: 8.0,
-      vote_count: 1500
-    },
-    {
-      budget: 100000000,
-      genres: [{ id: 28, name: 'Action' }, { id: 12, name: 'Adventure' }],
-      homepage: 'http://example.com',
-      id: 1,
-      keywords: [{ id: 1, name: 'example' }],
-      original_language: 'en',
-      original_title: 'Fallback Movie 1',
-      overview: 'This is a fallback movie.',
-      popularity: 10,
-      production_companies: [{ id: 1, name: 'Example Production' }],
-      production_countries: [{ iso_3166_1: 'US', name: 'United States' }],
-      release_date: '2024-01-01',
-      revenue: 5000000,
-      runtime: 120,
-      spoken_languages: [{ iso_639_1: 'en', name: 'English' }],
-      status: 'Released',
-      tagline: 'An example fallback movie.',
-      title: 'Fallback Movie 1',
-      vote_average: 7.0,
-      vote_count: 1000
-    },
-    {
-      budget: 200000000,
-      genres: [{ id: 16, name: 'Animation' }, { id: 35, name: 'Comedy' }],
-      homepage: 'http://example.com',
-      id: 2,
-      keywords: [{ id: 2, name: 'example' }],
-      original_language: 'en',
-      original_title: 'Fallback Movie 2',
-      overview: 'This is another fallback movie.',
-      popularity: 15,
-      production_companies: [{ id: 2, name: 'Example Studios' }],
-      production_countries: [{ iso_3166_1: 'GB', name: 'United Kingdom' }],
-      release_date: '2024-02-01',
-      revenue: 10000000,
-      runtime: 90,
-      spoken_languages: [{ iso_639_1: 'es', name: 'Spanish' }],
-      status: 'Released',
-      tagline: 'Another example fallback movie.',
-      title: 'Fallback Movie 2',
-      vote_average: 8.0,
-      vote_count: 1500
-    },
-    {
-      budget: 100000000,
-      genres: [{ id: 28, name: 'Action' }, { id: 12, name: 'Adventure' }],
-      homepage: 'http://example.com',
-      id: 1,
-      keywords: [{ id: 1, name: 'example' }],
-      original_language: 'en',
-      original_title: 'Fallback Movie 1',
-      overview: 'This is a fallback movie.',
-      popularity: 10,
-      production_companies: [{ id: 1, name: 'Example Production' }],
-      production_countries: [{ iso_3166_1: 'US', name: 'United States' }],
-      release_date: '2024-01-01',
-      revenue: 5000000,
-      runtime: 120,
-      spoken_languages: [{ iso_639_1: 'en', name: 'English' }],
-      status: 'Released',
-      tagline: 'An example fallback movie.',
-      title: 'Fallback Movie 1',
-      vote_average: 7.0,
-      vote_count: 1000
-    },
-    {
-      budget: 200000000,
-      genres: [{ id: 16, name: 'Animation' }, { id: 35, name: 'Comedy' }],
-      homepage: 'http://example.com',
-      id: 2,
-      keywords: [{ id: 2, name: 'example' }],
-      original_language: 'en',
-      original_title: 'Fallback Movie 2',
-      overview: 'This is another fallback movie.',
-      popularity: 15,
-      production_companies: [{ id: 2, name: 'Example Studios' }],
-      production_countries: [{ iso_3166_1: 'GB', name: 'United Kingdom' }],
-      release_date: '2024-02-01',
-      revenue: 10000000,
-      runtime: 90,
-      spoken_languages: [{ iso_639_1: 'es', name: 'Spanish' }],
-      status: 'Released',
-      tagline: 'Another example fallback movie.',
-      title: 'Fallback Movie 2',
-      vote_average: 8.0,
-      vote_count: 1500
-    }
-  ];
+  constructor(private movieService: MovieService, private router:Router, private route:ActivatedRoute) { }
 
 
   ngOnInit(): void {
-    this.fetchMovies();
+    const movieId = Number(this.route.snapshot.paramMap.get('id'));
+    this.fetchMovieDetails(movieId);
   }
 
-  fetchMovies(): void {
-    this.movieService.getTopRatedMovies().subscribe(
-      (data: Movie[]) => {
-        this.movies = data.map(movie => ({
-          ...movie,
-          poster_path: movie.poster_path ? `https://image.tmdb.org/t/p/w500${movie.poster_path}` : 'https://via.placeholder.com/500' // Use a placeholder if no image
-        }));
+  fetchMovieDetails(movieId: number): void {
+    this.movieService.getMovieDetails(movieId).subscribe(
+      (data) => {
+        this.movie = data;
+        console.log('Movie details:', this.movie);
+        this.fetchMovies();
       },
       (error) => {
-        console.error('Failed to fetch movies, using fallback list.', error);
-        this.movies = this.fallbackMovies;
+        console.error('Error fetching movie details:', error);
       }
     );
   }
-  
-  private movie: Movie={
-    budget: 100000000,
-      genres: [{ id: 28, name: 'Action' }, { id: 12, name: 'Adventure' }],
-      homepage: 'http://example.com',
-      id: 1,
-      keywords: [{ id: 1, name: 'example' }],
-      original_language: 'en',
-      original_title: 'Fallback Movie 1',
-      overview: 'This is a fallback movie.',
-      popularity: 10,
-      production_companies: [{ id: 1, name: 'Example Production' }],
-      production_countries: [{ iso_3166_1: 'US', name: 'United States' }],
-      release_date: '2024-01-01',
-      revenue: 5000000,
-      runtime: 120,
-      spoken_languages: [{ iso_639_1: 'en', name: 'English' }],
-      status: 'Released',
-      tagline: 'An example fallback movie.',
-      title: 'Fallback Movie 1',
-      vote_average: 7.0,
-      vote_count: 1000
-  };
+
+  fetchMovies(): void {
+    this.movieService.getRecommendedMovies(this.movie?.title as string, this.currentPage).subscribe(
+      (movies: Movie[]) => {
+        this.movies = movies;
+      },
+      (error) => {
+        console.error('Failed to fetch recommended movies.', error);
+      }
+    );
+  }
 
   goToDetails(movieId: number): void {
     this.router.navigate([`home/movies/`, movieId]);
   }
+  
+  
+  loadNextPage(): void {
+    this.currentPage++;
+    if (this.isSorted) {
+      this.sortResults();
+    } else {
+      this.fetchMovies();
+    } 
+  }
+
+  loadPreviousPage(): void {
+    if (this.currentPage > 1) { // Prevent going below page 1
+      this.currentPage--;
+      if (this.isSorted) {
+        this.sortResults();
+      } else {
+        this.fetchMovies();
+      } 
+    }
+  }
+
+  
+  sortResults() {
+  }
+
 }
